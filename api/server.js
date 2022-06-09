9e3598912472ed675f288f6945c241b4d902e09a
Andrew Gomez <drewgomez209@gmail.com>
2022-06-09 15:56:43 -0700

    finished

--- api/server.js
+++ api/server.js
@@ -19,30 +19,50 @@ server.use((err, req, res, next) => {
     })
 })
 
+// "username": "samwise",
+// "password": "gamgee"
+
 server.get('/api/users', (req, res) => {
-    Users.get()
+    Users.findAll()
         .then(result => {
             res.json(result)
         })
 })
 
 server.post('/api/register', (req, res) => {
-    let { username, password } = req.body
-    if (typeof username != 'string' || username != '') {
-        res.status(400).json({ message: 'invalid user' })
+    let { name, username, password } = req.body;
+    if (typeof username != 'string' || username === '') {
+        res.status(400).json({ message: 'invalid username' });
+        return;
     } else if (typeof password != 'string' || password === '') {
-        res.status(400).json({ message: "invalid password" })
-        return
+        res.status(400).json({ message: 'invalid password' });
+        return;
+    } else if (typeof name != 'string' || name === '') {
+        res.status(400).json({ message: 'invalid name' });
+        return;
     }
-    username = username.trim()
-    password = password.trim()
-    Users.add({ username: username, password: password })
+    name = name.trim();
+    username = username.trim();
+    password = password.trim();
+    Users.add({ name: name, username: username, password: password })
         .then(user => {
-            res.status(201).json(user)
+            res.status(201).json(user);
         })
-        .catch(err => {
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: 'Error adding the user' });
+        });
+});
 
-        })
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+    if (username == req.body.username && password == req.body.password) {
+        res.send({ message: `welcome ${req.body.username}!` })
+    } else {
+        console.log(error => {
+            res.status(500).json({ message: 'no username or password' });
+        });
+    }
 })
 
 module.exports = server
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const server = require('./api/server');
 
-
 console.log('process.env.PORT:', process.env.PORT)
 
 const PORT = process.env.PORT || 9000;
